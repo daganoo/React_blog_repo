3243bd92fbade57fcb2615740f2d5fc1d6964682
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2024-02-26 13:57:32 +0100

    workin on templat

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (  
+        <div>
+            <h1>Blogs</h1>
+            <a href="/">Home</a>
+            <a href="/">New Bolg</a>
+        </div>
+    );
+}
+ 
+export default Navbar;
