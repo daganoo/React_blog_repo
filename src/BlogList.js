5d5ba66064a2c7f1f1ffb322cc80cc12f5414da6
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2024-03-03 00:53:19 +0100

    add route Parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogList = ({ blogs, title }) => {
-  //const blogs = props.blogs;
-  //const title = props.title;
+  
   return (
     <div className="blog-list">
       <h2>{title}</h2>
