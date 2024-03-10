027cb90498fa9b3ef78b0c43936b8774b392be9f
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2024-03-10 22:44:09 +0000

    edit the form options

--- src/Create.js
+++ src/Create.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import useFetch from "./useFetch";
 import { useHistory } from "react-router-dom";
 
 const Create = () => {
@@ -48,8 +47,8 @@ const Create = () => {
         ></textarea>
         <label>Blog author :</label>
         <select value={author} onChange={(e) => setAuthor(e.target.value)}>
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
         </select>
         {!isPending && <button>Add Blog</button>}
         {isPending && <button>Adding...</button>}
